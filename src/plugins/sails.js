425c2a99150fba13f8eafb377905318075432c3e
Ashwin Chandrasekar <42815627+achandras@users.noreply.github.com>
2019-05-21 06:53:24 -0400

    Basic action wrapping for sails (#17)

---
+++ src/plugins/sails.js
@@ -0,0 +1,98 @@
+'use strict'
+
+const Tags = require('opentracing').Tags
+
+function wrapActionFunction (tracer, actionFn, identity) {
+  function actionWithTracing (req, res, next) {
+    const scope = tracer.scope()
+    const childOf = scope.active()
+    const span = tracer.startSpan(
+      actionFn.identity || 'action ' + identity,
+      {
+        childOf,
+        tags: {
+          [Tags.SPAN_KIND]: Tags.SPAN_KIND_RPC_SERVER,
+          'component': 'Sails.js'
+        }
+      }
+    )
+
+    try {
+      return scope.bind(actionFn, span).apply(this, arguments)
+    } catch (e) {
+      if (span) {
+        span.addTags({
+          'error': true,
+          'error.msg': e.message,
+          'error.stack': e.stack
+        })
+      }
+
+      throw e
+    } finally {
+      span.finish()
+    }
+  }
+
+  // copy over any additional members before returning, as by this point it's
+  // potentially a processed machine
+  return Object.assign(actionWithTracing, actionFn)
+}
+
+function wrapAction (tracer, action, identity) {
+  if (typeof action.fn === 'function') {
+    // actions2
+    action.fn = wrapActionFunction(tracer, action.fn, action.identity || identity)
+  } else {
+    // classic action
+    action = wrapActionFunction(tracer, action, identity)
+  }
+
+  return action
+}
+
+function wrapActionMiddleware (tracer, middleware, actionsGlobKey) {
+  if (typeof middleware === 'function') {
+    return wrapActionFunction(tracer, middleware, actionsGlobKey)
+  }
+
+  return middleware.map((fn) => {
+    return wrapActionFunction(tracer, fn, actionsGlobKey)
+  })
+}
+
+function createWrapRegisterAction (tracer, config) {
+  return function wrapRegisterAction (registerAction) {
+    return function registerActionWithTracing (action, identity, force) {
+      registerAction.call(this, wrapAction(tracer, action, identity), identity, force)
+    }
+  }
+}
+
+function createWrapRegisterActionMiddleware (tracer, config) {
+  return function wrapRegisterActionMiddleware (registerActionMiddleware) {
+    return function registerActionMiddlewareWithTracing (middleware, actionsGlobKey) {
+      registerActionMiddleware.call(this, wrapActionMiddleware(tracer, middleware, actionsGlobKey), actionsGlobKey)
+    }
+  }
+}
+
+function patchSails (sails, tracer, config) {
+  this.wrap(sails.prototype, 'registerAction', createWrapRegisterAction(tracer, config))
+  this.wrap(sails.prototype, 'registerActionMiddleware', createWrapRegisterActionMiddleware(tracer, config))
+}
+
+function unpatchSails (sails) {
+  this.unwrap(sails.prototype, 'registerAction')
+  this.unwrap(sails.prototype, 'registerActionMiddleware')
+}
+
+module.exports = [
+  {
+    name: 'sails',
+    file: 'lib/app/Sails.js',
+    versions: ['>=1'],
+    patch: patchSails,
+    unpatch: unpatchSails
+  }
+]
---
+++ test/plugins/sails.spec.js
@@ -0,0 +1,154 @@
+'use strict'
+
+const agent = require('./agent')
+const plugin = require('../../src/plugins/sails')
+
+wrapIt()
+
+describe('Plugin', () => {
+  let tracer
+  let Sails
+
+  describe('sails', () => {
+    withVersions(plugin, 'sails', version => {
+      beforeEach(() => {
+        tracer = require('../..')
+      })
+
+      before(() => {
+        return agent.load(plugin, 'sails').then(() => {
+          Sails = require(`../../versions/sails@${version}`).get()
+          Sails.config = {}
+          // Sails.load({environment: 'production'})
+        })
+      })
+
+      after(() => {
+        Sails.lower()
+
+        agent.close()
+      })
+
+      it('traces classic actions registered via registerAction', done => {
+        Sails.registerAction(() => { return 'test response' }, 'testAction')
+
+        agent.use(traces => {
+          expect(traces[0][0]).to.have.property('name', 'action testAction')
+          expect(traces[0][0]).to.have.property('service', 'test')
+          expect(traces[0][0].meta).to.have.property('component', 'Sails.js')
+          expect(traces[0][0].meta).to.have.property('span.kind', 'server')
+        }).then(done)
+          .catch(done)
+
+        expect(Sails.getActions()['testaction']()).to.eq('test response')
+      })
+
+      it('traces actions2 actions registered via registerAction', done => {
+        const mockRes = () => {
+          const res = {}
+          res.status = sinon.stub().returns(res)
+          res.json = sinon.stub().returns(res)
+          res.send = sinon.stub().returns(res)
+          res.sendStatus = sinon.stub().returns(res)
+          res.set = sinon.stub().returns(res)
+          return res
+        }
+        const functionToTrace = sinon.stub()
+        const action2dict = {
+          fn: functionToTrace
+        }
+
+        Sails.registerAction(action2dict, 'testAction2')
+
+        agent.use(traces => {
+          expect(traces[0][0]).to.have.property('name', 'action testAction2')
+          expect(traces[0][0]).to.have.property('service', 'test')
+          expect(traces[0][0].meta).to.have.property('component', 'Sails.js')
+          expect(traces[0][0].meta).to.have.property('span.kind', 'server')
+        }).then(done)
+          .catch(done)
+
+        expect(Sails.getActions()['testaction2']({}, mockRes()))
+        expect(functionToTrace).to.have.been.called
+      })
+
+      it('traces single actions registered via registerActionMiddleware', done => {
+        Sails.registerActionMiddleware(() => { return 'test response' }, 'testAction')
+
+        agent.use(traces => {
+          expect(traces[0][0]).to.have.property('name', 'action testAction')
+          expect(traces[0][0]).to.have.property('service', 'test')
+          expect(traces[0][0].meta).to.have.property('component', 'Sails.js')
+          expect(traces[0][0].meta).to.have.property('span.kind', 'server')
+        }).then(done)
+          .catch(done)
+
+        expect(Sails.getActions()['testaction']()).to.eq('test response')
+      })
+
+      it('traces arrays of actions registered via registerActionMiddleware', done => {
+        const actions = [
+          function () {
+            return 'test response'
+          }
+        ]
+        Sails.registerActionMiddleware(actions, 'testAction')
+
+        agent.use(traces => {
+          expect(traces[0][0]).to.have.property('name', 'action testAction')
+          expect(traces[0][0]).to.have.property('service', 'test')
+          expect(traces[0][0].meta).to.have.property('component', 'Sails.js')
+          expect(traces[0][0].meta).to.have.property('span.kind', 'server')
+        }).then(done)
+          .catch(done)
+
+        expect(Sails.getActions()['testaction']()).to.eq('test response')
+      })
+
+      it('tags errors from the action', done => {
+        Sails.registerAction(() => { throw new Error('fake error') }, 'testActionWithError')
+
+        agent.use(traces => {
+          expect(traces[0][0]).to.have.property('name', 'action testActionWithError')
+          expect(traces[0][0]).to.have.property('service', 'test')
+          expect(traces[0][0].meta).to.have.property('component', 'Sails.js')
+          expect(traces[0][0].meta).to.have.property('span.kind', 'server')
+          expect(traces[0][0].meta).to.have.property('error', 'true')
+          expect(traces[0][0].meta).to.have.property('error.msg', 'fake error')
+          expect(traces[0][0].meta).to.have.property('error.stack')
+        }).then(done)
+          .catch(done)
+
+        try {
+          Sails.getActions()['testactionwitherror']()
+        } catch (e) {
+          expect(e.message).to.eq('fake error')
+        }
+      })
+
+      it('propagates context', done => {
+        Sails.registerAction(() => { return 'actionWithPropagation' }, 'testActionWithPropagation')
+
+        const span = tracer.startSpan('parent span')
+        const scope = tracer.scope()
+
+        scope.bind(Sails)
+        scope.activate(span, () => {
+          agent.use(traces => {
+            expect(traces[0][0]).to.have.property('name', 'action testActionWithPropagation')
+            expect(traces[0][0]).to.have.property('service', 'test')
+            expect(traces[0][0]).to.have.property('parent_id', traces[0][1].trace_id)
+            expect(traces[0][0].meta).to.have.property('component', 'Sails.js')
+            expect(traces[0][0].meta).to.have.property('span.kind', 'server')
+          })
+            .then(done)
+            .catch(done)
+
+          Sails.getActions()['testactionwithpropagation']()
+        })
+
+        span.finish()
+      })
+    })
+  })
+})
