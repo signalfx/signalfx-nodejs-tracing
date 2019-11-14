7dbe3656bcf232734e03944c2e3f4395ed7a1008
Suzie Mae <smae@signalfx.com>
2019-11-14 13:20:42 -0500

    Knex auto-instumentation

--- src/plugins/knex.js
+++ src/plugins/knex.js
@@ -2,22 +2,129 @@
 
 const tx = require('./util/promise')
 
-function createPatch (file) {
-  return {
-    name: 'knex',
-    versions: ['>=0.8.0'],
-    file,
-    patch (Builder, tracer, config) {
-      this.wrap(Builder.prototype, 'then', tx.createWrapThen(tracer, config))
-    },
-    unpatch (Builder) {
-      this.unwrap(Builder.prototype, 'then')
+function createWrapQueryBuilderToSQL (tracer, config) {
+  return function wrapToSQL (toSQL) {
+    return function toSQLWithTrace (method, tz) {
+      const scope = tracer.scope()
+      const childOf = scope.active()
+      const mthd = method || this._method
+
+      const span = tracer.startSpan(`knex.QueryBuilder.toSQL(${mthd})`, {
+        childOf,
+        tags: {
+          'component': 'knex'
+        }
+      })
+      setDBTags(this, span)
+
+      return scope.activate(span, () => {
+        try {
+          return toSQL.apply(this, arguments)
+        } catch (e) {
+          throw addError(span, e)
+        } finally {
+          span.finish()
+        }
+      })
     }
   }
 }
 
+function createWrapSchemaBuilderToSQL (tracer, config) {
+  return function wrapToSQL (toSQL) {
+    return function toSQLWithTrace () {
+      const scope = tracer.scope()
+      const childOf = scope.active()
+      const span = tracer.startSpan('knex.SchemaBuilder.toSQL', {
+        childOf,
+        tags: {
+          'component': 'knex'
+        }
+      })
+      setDBTags(this, span)
+      setMethods(this, span)
+
+      return scope.activate(span, () => {
+        try {
+          return toSQL.apply(this, arguments)
+        } catch (e) {
+          throw addError(span, e)
+        } finally {
+          span.finish()
+        }
+      })
+    }
+  }
+}
+
+function addError (span, error) {
+  span.setTag('error', 'true')
+  span.log({
+    'error.type': error.name,
+    'error.msg': error.message,
+    'error.stack': error.stack
+  })
+  return error
+}
+
+function setDBTags (obj, span) {
+  if (obj.client && obj.client.config) {
+    const config = obj.client.config
+    if (config.client) {
+      span.setTag('db.type', config.client)
+    }
+    if (config.connection && config.connection.user) {
+      span.setTag('db.user', config.connection.user)
+    }
+  }
+}
+
+function setMethods (obj, span) {
+  if (obj._sequence) {
+    const methods = []
+    for (let i = 0, l = obj._sequence.length; i < l; i++) {
+      methods.push(obj._sequence[i].method)
+    }
+    span.setTag('schema.methods', methods)
+  }
+}
+
 module.exports = [
-  createPatch('lib/query/builder.js'),
-  createPatch('lib/raw.js'),
-  createPatch('lib/schema/builder.js')
+  {
+    name: 'knex',
+    versions: ['>=0.8.0'],
+    file: 'lib/query/builder.js',
+    patch (Builder, tracer, config) {
+      this.wrap(Builder.prototype, 'then', tx.createWrapThen(tracer, config))
+      this.wrap(Builder.prototype, 'toSQL', createWrapQueryBuilderToSQL(tracer, config))
+    },
+    unpatch (Builder) {
+      this.unwrap(Builder.prototype, 'then')
+      this.unwrap(Builder.prototype, 'toSQL')
+    }
+  },
+  {
+    name: 'knex',
+    versions: ['>=0.8.0'],
+    file: 'lib/schema/builder.js',
+    patch (Builder, tracer, config) {
+      this.wrap(Builder.prototype, 'then', tx.createWrapThen(tracer, config))
+      this.wrap(Builder.prototype, 'toSQL', createWrapSchemaBuilderToSQL(tracer, config))
+    },
+    unpatch (Builder) {
+      this.unwrap(Builder.prototype, 'then')
+      this.unwrap(Builder.prototype, 'toSQL')
+    }
+  },
+  {
+    name: 'knex',
+    versions: ['>=0.8.0'],
+    file: 'lib/raw.js',
+    patch (Raw, tracer, config) {
+      this.wrap(Raw.prototype, 'then', tx.createWrapThen(tracer, config))
+    },
+    unpatch (Raw) {
+      this.unwrap(Raw.prototype, 'then')
+    }
+  }
 ]
--- test/plugins/knex.spec.js
+++ test/plugins/knex.spec.js
@@ -18,7 +18,7 @@ describe('Plugin', () => {
         tracer = require('../..')
       })
 
-      afterEach(() => {
+      after(() => {
         return agent.close()
       })
 
@@ -31,19 +31,184 @@ describe('Plugin', () => {
                 client: 'sqlite3',
                 connection: {
                   filename: ':memory:'
-                }
+                },
+                useNullAsDefault: true
               })
             })
+      })
+
+        afterEach(() => {
+          client.schema.dropTableIfExists('testTable')
         })
-        it('should propagate context', () => {
-          if (process.env.DD_CONTEXT_PROPAGATION === 'false') return
+
+        it('should propagate context in the parent context', (done) => {
+          if (process.env.SIGNALFX_CONTEXT_PROPAGATION === 'false') return
 
           const span = {}
 
-          return tracer.scope().activate(span, () => {
-            return client.raw('PRAGMA user_version')
+          tracer.scope().activate(span, () => {
+            client.raw('PRAGMA user_version')
               .then(() => {
                 expect(tracer.scope().active()).to.equal(span)
+                done()
+              })
+          })
+        })
+
+        it('should automatically instrument schema builds', done => {
+          agent.use(traces => {
+            expect(traces[0][0]).to.have.property('service', 'test')
+            expect(traces[0][0]).to.have.property('name', 'knex.SchemaBuilder.toSQL')
+            expect(traces[0][0].meta).to.have.property('component', 'knex')
+            expect(traces[0][0].meta).to.have.property('schema.methods', 'createTable')
+            expect(traces[0][0].meta).to.have.property('db.type', 'sqlite3')
+
+            done()
+          })
+          .catch(done)
+
+          client.schema.createTable('testTable', (table) => {
+            table.integer('id')
+            table.string('title')
+          }).then(() => {})
+      })
+
+        it('should automatically instrument simple queries', done => {
+          agent.use(traces => {
+
+            const spans = traces[0]
+
+            expect(spans).to.have.length(4)
+
+            expect(spans[0]).to.have.property('service', 'test')
+            expect(spans[0]).to.have.property('name', 'knex.SchemaBuilder.toSQL')
+            expect(spans[0].meta).to.have.property('component', 'knex')
+            expect(spans[0].meta).to.have.property('db.type', 'sqlite3')
+            expect(spans[0].meta).to.have.property('schema.methods', 'createTable')
+            expect(spans[0].parent_id.toString()).to.equal(spans[3].span_id.toString())
+
+            expect(spans[1]).to.have.property('service', 'test')
+            expect(spans[1]).to.have.property('name', 'knex.QueryBuilder.toSQL(insert)')
+            expect(spans[1].meta).to.have.property('component', 'knex')
+            expect(spans[1].meta).to.have.property('db.type', 'sqlite3')
+            expect(spans[1].parent_id.toString()).to.equal(spans[3].span_id.toString())
+
+            expect(spans[2]).to.have.property('service', 'test')
+            expect(spans[2]).to.have.property('name', 'knex.QueryBuilder.toSQL(select)')
+            expect(spans[2].meta).to.have.property('component', 'knex')
+            expect(spans[2].meta).to.have.property('db.type', 'sqlite3')
+            expect(spans[2].parent_id.toString()).to.equal(spans[3].span_id.toString())
+
+            expect(spans[3]).to.have.property('service', 'test')
+            expect(spans[3]).to.have.property('name', 'testSpan')
+            done()
+          })
+          .catch(done)
+
+          const span = tracer.startSpan('testSpan')
+
+          tracer.scope().activate(span, () => {
+            client.schema
+            .createTable('testTable', (table) => {
+              table.integer('id')
+              table.string('title')
+            })
+            .then(() => {
+              return client.insert({id: 1, title: 'knex test'}).into('testTable');
+            })
+            .then(() => {
+                return client('testTable').select('*')
+                  .then(() => {
+                    span.finish()
+                  })
+              })
+          })
+        })
+
+        it('should automatically instrument complex queries', done => {
+          agent.use(traces => {
+            const spans = traces[0]
+
+            expect(spans).to.have.length(5)
+
+            expect(spans[0]).to.have.property('service', 'test')
+            expect(spans[0]).to.have.property('name', 'knex.SchemaBuilder.toSQL')
+            expect(spans[0].meta).to.have.property('component', 'knex')
+            expect(spans[0].meta).to.have.property('db.type', 'sqlite3')
+            expect(spans[0].meta).to.have.property('schema.methods', 'createTable,createTable')
+            expect(spans[0].parent_id.toString()).to.equal(spans[4].span_id.toString())
+
+            expect(spans[1]).to.have.property('service', 'test')
+            expect(spans[1]).to.have.property('name', 'knex.QueryBuilder.toSQL(insert)')
+            expect(spans[1].meta).to.have.property('component', 'knex')
+            expect(spans[1].meta).to.have.property('db.type', 'sqlite3')
+            expect(spans[1].parent_id.toString()).to.equal(spans[4].span_id.toString())
+
+            expect(spans[2]).to.have.property('service', 'test')
+            expect(spans[2]).to.have.property('name', 'knex.QueryBuilder.toSQL(insert)')
+            expect(spans[2].meta).to.have.property('component', 'knex')
+            expect(spans[2].meta).to.have.property('db.type', 'sqlite3')
+            expect(spans[2].parent_id.toString()).to.equal(spans[4].span_id.toString())
+
+            expect(spans[3]).to.have.property('service', 'test')
+            expect(spans[3]).to.have.property('name', 'knex.QueryBuilder.toSQL(select)')
+            expect(spans[3].meta).to.have.property('component', 'knex')
+            expect(spans[3].meta).to.have.property('db.type', 'sqlite3')
+            expect(spans[3].parent_id.toString()).to.equal(spans[4].span_id.toString())
+
+            expect(spans[4]).to.have.property('service', 'test')
+            expect(spans[4]).to.have.property('name', 'testSpan')
+
+            done()
+          })
+        .catch(done)
+
+          const span = tracer.startSpan('testSpan')
+          tracer.scope().activate(span, () => {
+            client.schema
+              .createTable('users', function(table) {
+                table.increments('id');
+                table.string('user_name');
+              })
+              .createTable('accounts', function(table) {
+                table.increments('id');
+                table.string('account_name');
+                table.integer('user_id').unsigned().references('users.id');
+              })
+              .then(function() {
+                return client.insert({user_name: 'Tim'}).into('users');
+              })
+              .then(function(rows) {
+                return client.table('accounts').insert({account_name: 'knex', user_id: rows[0]});
+              })
+              .then(function() {
+                return client('users')
+                  .join('accounts', 'users.id', 'accounts.user_id')
+                  .select('users.user_name as user', 'accounts.account_name as account')
+                  .then (() => {
+                    span.finish()
+                  })
+               })
+            })
+        })
+
+        it('should propagate context to callbacks', done => {
+          if (process.env.SIGNALFX_CONTEXT_PROPAGATION === 'false') return done()
+
+          const span = tracer.startSpan('test')
+
+          tracer.scope().activate(span, () => {
+            const span = tracer.scope().active()
+            client.schema.createTable('testTable', (table) => {
+              table.integer('id')
+              table.string('title')
+            })
+            .asCallback((err) => {
+                if (err) throw err
+
+                expect(tracer.scope().active()).to.equal(span)
+                span.finish()
+                done()
               })
           })
         })
